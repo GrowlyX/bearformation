c898a5b762e8965c3a00624bdd5ed291d22ea443
egirldevs <emily@Bunni.me>
2023-11-11 23:20:14 -0500

    Enhance installation script
    - Update and upgrade system packages
    - Install and configure Starship prompt
    - Configure SSH settings for better security
    - Set up UFW rules for SSH access and Docker
    - Install and configure Docker with UFW rules
    - Configure MongoDB, Redis, and Consul (optional)
    - Miscellaneous improvements and bug fixes
    
    This commit refines the installation script, addressing security considerations, and optimizing various configurations for a more robust setup.

--- completeInstall.sh
+++ completeInstall.sh
@@ -1,114 +1,107 @@
 #!/bin/bash
 
-# author - growlyx
-# update & upgrade if required
-apt-get update
-apt-get upgrade
-
-# install & configure starship
-curl -sS https://starship.rs/install.sh | sh
-
-# add starship evaluator to .bashrc
-echo eval "$(starship init bash)" >>".bashrc"
-source .bashrc
-
-echo "configured starship"
-
-# install bashtop, because it's cool
-snap install bashtop
-
-# configure SSH, enable pubkey auth, & disable pass auth
-cp -R resources/sshd_config /etc/ssh
-mkdir /.ssh/
-touch /.ssh/authorized_keys
-
-echo "enter your ssh key, more can be added in \"/.ssh/authorized_keys\" (We assume all users are on root)"
-read -r ssh_key
-
-echo "$ssh_key" >>/.ssh/authorized_keys
-
-systemctl restart sshd
-echo "configured ssh (please test)"
-
-# configure default UFW rules
-current_ip=$(echo "$SSH_CLIENT" | cut -d' ' -f 1)
-
-ufw allow 22
-ufw allow in from ${current_ip}
-ufw default deny incoming
-ufw enable
-
-echo "we've configured ufw and allowed your IP to access services."
-
-# install & configure docker.io & UFW rules
-echo "should we configure docker & its UFW rules?"
-read -r configure_docker_ufw
-
-if [ "$configure_docker_ufw" = true ]; then
-  apt install docker.io
-
-  # disable docker default iptables configuration
-  mkdir /etc/docker
-  touch /etc/docker/daemon.json
-
-  echo "{ \"iptables\": false }" >>/etc/docker/daemon.json
-
-  # set default FORWARD policy to accept & reload
-  sed -i -e 's/DEFAULT_FORWARD_POLICY="DROP"/DEFAULT_FORWARD_POLICY="ACCEPT"/g' /etc/default/ufw
-  ufw reload
-
-  iptables -t nat -A POSTROUTING ! -o docker0 -s 172.17.0.0/16 -j MASQUERADE
-  systemctl restart docker
-
-  echo "configured docker"
+if [ ! "$EUID" -eq 0 ]; then
+    echo "This script must be run as root."
+    exit 1
 fi
+# Function to update and upgrade
+update_upgrade() {
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+}
 
-# install & configure redis
-echo "should we configure redis?"
-read -r configure_redis
+# Function to install Starship
+install_starship() {
+    if ! command -v starship &> /dev/null; then
+        sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -f
+        echo 'eval "$(starship init bash)"' >> ~/.bashrc
+        source ~/.bashrc
+        echo "Configured Starship"
+    fi
+}
 
-if [ "$configure_redis" = true ]; then
-  sudo apt install lsb-release
-  curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
-  echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
-  sudo apt-get update
-  sudo apt-get install redis
+# Function to configure SSH
+configure_ssh() {
+    mkdir -p ~/.ssh
+    touch ~/.ssh/authorized_keys
 
-  cp -R resources/redis.conf /etc/redis/
-  systemctl restart redis
+    echo "Enter your SSH key:"
+    read -r ssh_key
 
-  echo "configured redis"
-fi
+    echo "$ssh_key" >> ~/.ssh/authorized_keys
+    systemctl restart sshd
+    echo "Configured SSH (please test in a new ssh session to verify)"
+}
 
-# install & configure mongo
-echo "should we configure mongo?"
-read -r configure_mongo
+# Function to configure SSH settings
+configure_ssh_settings() {
+    sshd_config="/etc/ssh/sshd_config"
 
-if [ "$configure_mongo" = true ]; then
-  sudo apt-get install gnupg
-  curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
-     sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg \
-     --dearmor
-  echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+    # Backup the original sshd_config file
+    sudo cp "$sshd_config" "$sshd_config.bak"
 
-  sudo apt-get update
-  sudo apt-get install -y mongodb-org
+    # Update SSH settings
+    sudo sed -i 's/#LogLevel INFO/LogLevel VERBOSE/' "$sshd_config"
+    sudo sed -i 's/#MaxAuthTries 6/MaxAuthTries 2/' "$sshd_config"
+    sudo sed -i 's/#MaxSessions 10/MaxSessions 2/' "$sshd_config"
+    sudo sed -i 's/#AllowAgentForwarding yes/AllowAgentForwarding no/' "$sshd_config"
+    sudo sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding no/' "$sshd_config"
+    sudo sed -i 's/#TCPKeepAlive yes/TCPKeepAlive no/' "$sshd_config"
+    sudo sed -i 's/#Compression delayed/Compression delayed/' "$sshd_config"
+    sudo sed -i 's/#ClientAliveCountMax 3/ClientAliveCountMax 2/' "$sshd_config"
+    sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' "$sshd_config"
 
-  cp -R resources/mongod.conf /etc/
-  systemctl restart mongod
+    # Restart SSH service to apply changes
+    sudo systemctl restart sshd
 
-  echo "configured mongo"
-fi
+    echo "Configured SSH settings"
+}
 
-# install & configure consul
-echo "should we start up a consul dev server (requires docker)?"
-read -r configure_consul
 
-if [ "$configure_consul" = true ]; then
-  # start consul dev server @ 0.0.0.0:8500
-  docker run -d --name=dev-consul -e CONSUL_BIND_INTERFACE=eth0 -p 8500:8500 consul
+# Function to configure UFW rules
+configure_ufw() {
+    current_ip=$(echo "$SSH_CLIENT" | cut -d' ' -f 1)
 
-  echo "configured consul"
-fi
+    ufw allow 22 comment 'Allow SSH'
+    ufw allow in from "${current_ip}" comment 'Allow current IP'
+    ufw default deny incoming
+    ufw enable
 
-echo "woo hoo finished installation"
+    echo "Configured UFW and allowed your IP to access services."
+}
+
+# Function to configure Docker and UFW rules
+configure_docker_ufw() {
+    echo "Do you want to configure Docker & its UFW rules?"
+    read -r configure_docker_ufw
+
+    if [ "$configure_docker_ufw" = true ]; then
+        if ! command -v docker &> /dev/null; then
+            sudo apt install docker.io -y
+        fi
+
+        # Disable docker default iptables configuration
+        mkdir -p /etc/docker
+        echo "{ \"iptables\": false }" >> /etc/docker/daemon.json
+
+        # Set default FORWARD policy to accept & reload
+        sed -i -e 's/DEFAULT_FORWARD_POLICY="DROP"/DEFAULT_FORWARD_POLICY="ACCEPT"/g' /etc/default/ufw
+        ufw reload
+
+        iptables -t nat -A POSTROUTING ! -o docker0 -s 172.17.0.0/16 -j MASQUERADE
+        systemctl restart docker
+
+        echo "Configured Docker"
+    fi
+}
+
+
+# Main execution
+update_upgrade
+install_starship
+configure_ssh
+configure_ssh_settings
+configure_ufw
+configure_docker_ufw
+
+echo "Installation script completed."
